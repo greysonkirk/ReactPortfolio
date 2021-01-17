e6d8969cf2d3847975c4220e1f8db327dc2890b1
Greyson Kirk <greysonkirk@yahoo.com>
2021-01-16 19:32:36 -0600

    carousel working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,66 @@
 import React, { useEffect, useState } from "react";
 // import { Link, useParams } from "react-router-dom";
 import { Col, Row, Container } from "../Components/Grid";
-import API from '../utils/API'
-import Me from '../me.jpg'
-// import API from "../utils/API";
+import {repos} from "../utils/repos";
+import Me from "../me.jpg";
+import Carousel from 'react-bootstrap/Carousel'
  
+
+// import API from "../utils/API";
 
 function Home(props) {
+  const [gitRepo, setgitRepo] = useState([]);
+useEffect(() => {
+  setgitRepo(repos)
+}, [])
 
- const [gitRepo, setgitRepo] = useState([{}])
 
- useEffect(() => {
-  API.getProjects()
-  .then(res => 
-    setgitRepo(res.data)
-  )
-  .catch(err => console.log(err));
- },[])
+  console.log(gitRepo);
 
- console.log(gitRepo)
- 
   return (
     <Container fluid>
       <Row>
         <Col size="md-12">
- 
-            <h1 className="text-light"> I'm Greyson. 
-               <p><img style={{borderRadius: '50%', marginTop:'15px'}} src={Me} height="350px" alt="me"></img> </p>
-               a full stack developer
-            </h1>
-        
+          <h1 className="text-light">
+            {" "}
+            I'm Greyson.
+            <p>
+              <img
+                style={{ borderRadius: "50%", marginTop: "15px" }}
+                src={Me}
+                height="350px"
+                alt="me"
+              ></img>{" "}
+            </p>
+            a full stack developer
+          </h1>
         </Col>
       </Row>
       <Container>
         <Row>
-    
-        <Col size="6">
- stuf <p>{gitRepo[0].name}</p>
-  </Col>
-      </Row>
+          <Col size="6">
+            <Carousel>
+              {repos.map((repo) => {
+                return(
+              <Carousel.Item interval={1000}>
+                <img
+                key={repo.id}
+                  className="d-block w-100"
+                  src={repo.img}
+                  alt= {repo.name}
+                />
+                <Carousel.Caption>
+                  <h3>{repo.name}</h3>
+                  <p>
+                    {repo.desc}
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              )
+              })}
+            </Carousel>
+          </Col>
+        </Row>
       </Container>
     </Container>
   );
---
+++ src/utils/repos.js
@@ -0,0 +1,58 @@
+
+import bgex from "../repos/BurgerExpress.PNG";
+import ct from "../repos/ct.PNG";
+import cq from "../repos/codeQuiz.png";
+import ed from "../repos/EmpDirec.png";
+import nt from "../repos/nt.PNG";
+import wa from "../repos/wa.PNG";
+
+export const  repos = [
+  {
+    id: 1,
+    name: "Burger Express",
+    link: "https://github.com/greysonkirk/BurgerExpress",
+    desc:
+      "This fun handlebars exercise allows users to input a burger, devour it, throw it up, or delete it. ",
+    img: bgex 
+  },
+  {
+    id: 2,
+    name: "Chicken Tinder",
+    link: "https://github.com/greysonkirk/ChickenTinder",
+    desc:
+      "This app uses express, node, handlebars, sequelize, and a MySql db. This app allows a set of users go through a list of restaurants in their area, and once both choose the same restaurants, they are notified.",
+    img: ct
+  },
+  {
+    id: 3,
+    name: "Code Quiz",
+    link: "https://github.com/greysonkirk/CodeQuiz",
+    desc:
+      "A fun coding quiz that decrements the time left if the answer is wrong!",
+    img: cq 
+  },
+  {
+    id: 4,
+    name: "Employee Directory",
+    link: "https://github.com/greysonkirk/EmployeeDirectory",
+    desc:
+      "React app that pulls 100 random employees from randomuser.me. User is able to search and sort. ",
+    img: ed
+  },
+  {
+    id: 5,
+    name: "Note Taker",
+    link: "https://github.com/greysonkirk/NoteTaker",
+    desc:
+      "This app helps users take, keep, and delete notes easily. User will input a note and click to the save icon to save a note and if they decide to delete the note, they can click the trashcan icon.",
+    img: nt
+  },
+  {
+    id: 6,
+    name: "Weather App",
+    link: "https://github.com/greysonkirk/WeatherApp",
+    desc:
+      "A weather app that allows users to search for a city and see the current weather as well as a five day forcast.",
+    img: wa 
+  },
+];
